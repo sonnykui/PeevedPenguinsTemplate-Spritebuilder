62872e795f28f4712a67134cb9a65659f454abd9
sonnykui <sonnykui@yahoo.com>
2014-12-05 23:29:22 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Penguin.m
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -8,15 +8,6 @@
 
 #import "Penguin.h"
 
-@implementation Penguin
 
-- (id)init {
-    self  = [super init];
-    
-    if (self) {
-        CCLOG(@"Penguin created");
-    }
-    return self;
-}
 
 @end
--- PeevedPenguins.spritebuilder/Source/Seal.m
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -10,13 +10,6 @@
 
 @implementation Seal
 
-- (id)init {
-    self = [super init];
-    
-    if (self) {
-        CCLOG(@"Seal created");
-    }
-    return self;
-}
+
 
 @end
